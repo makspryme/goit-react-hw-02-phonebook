8d565f9dc99674cd01647c6e6c2ba31b9b5aa2b1
makspryme <makspryme17@gmail.com>
2023-09-25 23:48:22 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  uniqueId = nanoid();
+
+  handleInputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmitForm = e => {
+    e.preventDefault();
+    this.setState(prevState => {
+      prevState.contacts.push({
+        id: nanoid(),
+        name: prevState.name,
+        number: prevState.number,
+      });
+    });
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form className="form-contact" onSubmit={this.handleSubmitForm}>
+          <label className="label-contact">
+            Name
+            <input
+              type="text"
+              name="name"
+              required
+              value={this.state.name}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label className="label-contact">
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <button className="btn-submit" type="submit">
+            Add Contact
+          </button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(el => {
+            return <li key={nanoid()}>{`${el.name} - ${el.number}`}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
